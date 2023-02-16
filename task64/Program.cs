2970b76144b21fa95653ce479a264b6aaaf7ebc6
bananan <truckrub@gmail.com>
2023-02-16 16:08:12 +0300

    add task64

---
+++ task64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от N до 1.
+
+void PrintNumbers(int n)
+{
+
+Console.Write(n+ " ");
+    if(n==1) return;
+PrintNumbers(n-1);
+}
+int number = 1999;
+PrintNumbers(number);
+Console.WriteLine();
+Console.WriteLine("it's simple (:");
+Console.ReadKey();
+
